822fa2741797e9ec740a6ed3ab307d8d6f90a499
Abhishek.Vidyarthi <abhishek.vidyarthi@maersk.com>
2022-11-07 14:17:01 +0100

    Update Products functionality

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -50,11 +50,12 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 return View(productVM);
             }
             else
-            { 
-                //Update Product
+            {
+                productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+                return View(productVM);
             }
             
-            return View(productVM);
+            
         }
 
         //POST
@@ -72,12 +73,29 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extension = Path.GetExtension(file.FileName);
 
+                    if(obj.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
                     using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                     {
                         file.CopyTo(fileStreams);
                     }
                     obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
                 }
+                if(obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+                }
+
                 //_unitOfWork.CoverType.Update(obj);
                 _unitOfWork.Product.Add(obj.Product);   
                 _unitOfWork.Save();
