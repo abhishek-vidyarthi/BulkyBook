3435aad0f4687623315b5dc2fd64abe7060c845f
Abhishek.Vidyarthi <abhishek.vidyarthi@maersk.com>
2023-10-03 14:54:32 +0200

    Add order confirmation

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -164,8 +164,19 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 		public IActionResult OrderConfirmation(int id)
 		{
 			OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == id);
-
+			var service = new SessionService();
+			Session session = service.Get(orderHeader.SessionId);
 			//check the stripe status
+			if (session.PaymentStatus.ToLower() == "paid")
+			{
+				_unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
+				_unitOfWork.Save();
+			}
+			List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId ==
+			orderHeader.ApplicationUserId).ToList();
+			_unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+			_unitOfWork.Save();
+			return View(id);
 		}
 
 		public IActionResult Plus(int cartId)
