accdd092a4d77c0786ce7f6955e67e18b7eeca6d
Abhishek.Vidyarthi <abhishek.vidyarthi@maersk.com>
2023-10-30 14:37:40 +0100

    Order Details page & update method

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,59 +10,97 @@ using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Authorize]
-    public class OrderController : Controller
-    {
-        private readonly IUnitOfWork _unitOfWork;
-        public OrderController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
+	[Area("Admin")]
+	[Authorize]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		[BindProperty]
+		public OrderVM OrderVM { get; set; }
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        #region API CALLS
-        [HttpGet]
-        public IActionResult GetAll(string status)
-        {
-            IEnumerable<OrderHeader> orderHeaders;
+		public IActionResult Details(int orderId)
+		{
+			OrderVM = new OrderVM()
+			{
+				OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+				OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == orderId, includeProperties: "Product"),
+			};
+			return View(OrderVM);
+		}
 
-            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
-            {
-                orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
-            }
-            else
-            {
-                var claimsIdentity = (ClaimsIdentity)User.Identity;
-                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-                orderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
-            }
-
-            switch (status)
-            {
-                case "pending":
-                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
-                    break;
-                case "inprocess":
-                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
-                    break;
-                case "completed":
-                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
-                    break;
-                case "approved":
-                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
-                    break;
-                default:
-                    break;
-            }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult UpdateOrderDetail()
+		{
+			var orderHEaderFromDb = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, tracked: false);
+			orderHEaderFromDb.Name = OrderVM.OrderHeader.Name;
+			orderHEaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+			orderHEaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+			orderHEaderFromDb.City = OrderVM.OrderHeader.City;
+			orderHEaderFromDb.State = OrderVM.OrderHeader.State;
+			orderHEaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+			if (OrderVM.OrderHeader.Carrier != null)
+			{
+				orderHEaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+			}
+			if (OrderVM.OrderHeader.TrackingNumber != null)
+			{
+				orderHEaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+			}
+			_unitOfWork.OrderHeader.Update(orderHEaderFromDb);
+			_unitOfWork.Save();
+			TempData["Success"] = "Order Details Updated Successfully.";
+			return RedirectToAction("Details", "Order", new { orderId = orderHEaderFromDb.Id });
+		}
 
 
-            return Json(new { data = orderHeaders });
-        }
-        #endregion
-    }
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll(string status)
+		{
+			IEnumerable<OrderHeader> orderHeaders;
+
+			if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+			{
+				orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+			}
+			else
+			{
+				var claimsIdentity = (ClaimsIdentity)User.Identity;
+				var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+				orderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
+			}
+
+			switch (status)
+			{
+				case "pending":
+					orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+					break;
+				case "inprocess":
+					orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+					break;
+				case "completed":
+					orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+					break;
+				case "approved":
+					orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+					break;
+				default:
+					break;
+			}
+
+
+			return Json(new { data = orderHeaders });
+		}
+		#endregion
+	}
 }
